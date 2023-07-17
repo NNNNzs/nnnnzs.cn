d99fa6c66143d5a2ac91d1422a10e5ad63d27667
NNNNzs <nnnnzs@vip.qq.com>
2023-07-17 15:00:10 +0800

    feat: 日志查看功能

--- pages/c/tools/log.vue
+++ pages/c/tools/log.vue
@@ -1,17 +1,14 @@
 <template>
   <ClientOnly>
     <div class="w-full h-full flex flex-col p-4 bg-blue-50">
+      <el-form>
+        <el-date-picker v-model="timeRange" type="daterange" range-separator="To" start-placeholder="Start date"
+          end-placeholder="End date" size="small" value-format="YYYY-MM-DD" />
+      </el-form>
       <div class="flex-1 overflow-hidden">
         <ElTable :data="tableList" border height="100%">
-          <ElTableColumn v-for="item in tableHeader" :key="item.prop" :label="item.prop" :prop="item.prop"
+          <ElTableColumn v-for="item in tableHeader" :key="item.prop" :label="item.label" :prop="item.prop"
             :formatter="item.formatter || undefined">
-          </ElTableColumn>
-          <ElTableColumn prop="edit" label="操作">
-            <template #default="{ row }">
-              <el-button link type="primary" size="small" @click="show(row)">查看</el-button>
-              <el-button link type="primary" size="small" @click="edit(row)">编辑</el-button>
-              <el-button link type="primary" size="small" @click="handleDelete(row)">删除</el-button>
-            </template>
           </ElTableColumn>
         </ElTable>
       </div>
@@ -25,87 +22,57 @@
 <script setup lang="ts">
 
 
-import { ElTable, ElTableColumn, ElButton, ElPagination, ElInput } from 'element-plus'
-
+import { ElTable, ElTableColumn, ElForm, ElPagination, ElDatePicker, type DateModelType } from 'element-plus'
 import dayjs from 'dayjs'
 
-// @ts-ignore
-import { AisInstantSearch, AisSearchBox, AisHits } from 'vue-instantsearch/vue3/es'
-
-const { result, search } = useAlgoliaSearch('nnnnzs')
-
-interface Query extends QueryCondition {
-  hide: string,
-  query: string
+interface Query extends QueryCondition, Partial<Log> {
+  startTime: string
+  endTime: string
 }
 
-
 const query = reactive<Query>({
-  pageSize: 10,
+  pageSize: 30,
   pageNum: 1,
-  hide: 'all',
-  query: '',
+  startTime: dayjs().subtract(7, 'day').format('YYYY-MM-DD'),
+  endTime: dayjs().add(1, 'day').format('YYYY-MM-DD')
 });
-
-watch(() => query.query, (newVal) => {
-  search({ query: newVal });
-  console.log(result.value)
-});
+const timeRange = computed({
+  get() {
+    return [query.startTime, query.endTime] as [DateModelType, DateModelType]
+  },
+  set(newVal) {
+    query.startTime = newVal[0] as string
+    query.endTime = dayjs(newVal[1]).add(1, 'day').format('YYYY-MM-DD') as string;
+  }
+})
 
 const pageTotal = ref(0)
-const tableList = ref<Post[]>([])
+const tableList = ref<Partial<Log>[]>([])
 
 const tableHeader = ref([
   { prop: 'id', label: "id" },
-  { prop: 'title', label: "标题" },
+  { prop: 'value1', label: "value1" },
+  { prop: 'value2', label: "value2" },
+  { prop: 'value3', label: "value3" },
+  { prop: 'value4', label: "value4" },
   {
-    prop: 'date', label: "date", formatter(row: Post) {
-      return dayjs(row.date).format('YYYY-MM-DD HH:mm:ss')
+    prop: 'time', label: "时间", formatter(row: Log) {
+      return dayjs(row.time).format('YYYY-MM-DD HH:mm:ss')
     }
   },
-  { prop: 'tags', label: "tags" },
-  {
-    prop: 'hide', label: "隐藏", formatter(row: Post) {
-      if (row.hide === '1') {
-        return '隐藏'
-      }
-      return '显示'
-    }
-  },
-  { prop: 'path', label: "路径" },
-  { prop: 'description', label: "描述" },
+  { prop: 'ref', label: "ref" },
 ])
 
-const show = (row: Post) => {
-  window.open(row.path)
-}
-const edit = (row: Post) => {
-  window.open(EDIT_PAGE + row.id)
-}
-
-const addPost = () => {
-  window.open(EDIT_PAGE + 'edit');
-};
-
-const handleDelete = (row: Post) => {
-  if (row.id) {
-    $fetch('/api/post/del', {
-      method: 'DELETE',
-      query: { id: row.id }
-    })
-      .then(res => {
-        if (res.status) {
-          getList()
-        }
-      })
-  }
-}
-
 const getList = async () => {
-  const res = await $fetch('/api/post/list', { query });
-  pageTotal.value = res.total;
-  tableList.value = res.record;
+  const res = await $fetch(clientUrl + '/log/list', { query }) as ResponeBody<PageQueryRes<Log>>
+  pageTotal.value = res.data.total;
+  tableList.value = res.data.record;
 }
+
+
+watchEffect(() => {
+  getList()
+})
 
 
 </script>
---
+++ types/log.d.ts
@@ -0,0 +1,16 @@
+
+declare interface Log {
+  id: number;
+
+  time: string;
+
+  value1: string;
+
+  value2: string;
+
+  value3: string;
+
+  value4: string;
+
+  ref: string;
+}
