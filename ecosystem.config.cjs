48421ff22b54bceb1d8cba3105396977a03ce5f3
NNNNzs <nnnnzs@vip.qq.com>
2024-06-02 22:27:05 +0800

    feat: 添加docker-compose

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,8 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'nnnnzs.cn',
-      script: 'npm run start',
-      interpreter: "nvm use 20 && node",
+      script: 'npm run start'
     },
   ],
 }
